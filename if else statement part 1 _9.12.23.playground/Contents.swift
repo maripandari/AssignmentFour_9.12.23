59d508cadef38afafb647c5442d1851f92e552c0
Marijoy Jatico <142898185+maripandari@users.noreply.github.com>
2023-09-12 13:30:54 -1000

    Completed Parts 1-4
    Still need to complete On your Own

--- if else statement part 1 _9.12.23.playground/Contents.swift
+++ if else statement part 1 _9.12.23.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//Assignment #4
+//Part 2
+
+var goodGrade = "I've been doing all my work"
+
+//create our first if statement
+//this  is out conditional statement
+
+if goodGrade == "I've been doing all my work"
+{
+    
+    //if block
+    print("I should be getting an A at this point")
+}
+//Part 3
+else
+{
+    //else block
+    print("I need to work harder in this class")
+}
+
+//Part 4
+var gradeA = true
+
+if gradeA == true
+{
+    print("I really like this class")
+    
+}
+else
+{
+    print("I'm not sure about coding in Swift")
+    
+}
+
